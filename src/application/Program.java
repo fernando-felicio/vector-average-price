0b1ba0b7e54ae83a0a3b05479b21776a5bf663ae
Fernando Felício <fernandofelicio.dev@gmail.com>
2024-01-02 11:03:17 -0300

    Creating business rules

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 import java.util.Locale;
 import java.util.Scanner;
+import entities.Product;
 
 public class Program {
 	
@@ -11,8 +12,27 @@ public class Program {
 		
 		System.out.println("How many products will be added ?");
 		int n = scanner.nextInt();
+		Product[] vect = new Product[n];
 		
+				
+		for (int i = 0; i < vect.length; i++) {
+			scanner.nextLine();
+			System.out.println("What is the name and value of product number " + (i + 1) + "?");
+			System.out.println("Name: "); 
+			String name = scanner.nextLine();
+			System.out.println("Price: ");
+			double price = scanner.nextDouble();
+			vect[i] = new Product(name, price);
+		}
 		
+		double sum = 0.0;
+		for (int i=0; i<vect.length; i++) {
+			sum	+= vect[i].getPrice();
+		}
+		
+		double average = sum / vect.length;
+		
+		System.out.printf("AVERAGE PRICE = %.2f%n", average);
 		
 		
 		
