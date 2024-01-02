852c92f0b3a6403d7e1678997bf37741a3c44461
Fernando Felício <fernandofelicio.dev@gmail.com>
2024-01-02 10:36:14 -0300

    Creating Product class

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main (String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("How many products will be added ?");
+		int n = scanner.nextInt();
+		
+		
+		
+		
+		
+		scanner.close();
+		
+	}
+
+}
